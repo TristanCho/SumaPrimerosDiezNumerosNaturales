cdaaa68d95aba3a3cfe76ddb9821d6cbe205da10
TristanCho <51831659+TristanCho@users.noreply.github.com>
2019-10-17 23:44:32 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaPrimerosDiezNumerosNaturales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String TextN1, TextN2, TextN3, TextN4, TextN5, TextN6, TextN7, TextN8, TextN9, TextN10;
+            Console.WriteLine("A continuación debes introducir los primeros 10 números naturales para comenzando por el 1 hasta el 10");
+        }
+    }
+}
